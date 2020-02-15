edd826f3a2e458886e40418b23a326d6e9e1de89
Marcelo <marcelo18a@msn.com>
2020-02-14 22:08:51 -0300

    add environment variables to docker

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '%$qcdbmywd(qhnllsq!v86b1vyw6%+b3sxaw@8!m$^z*cubet9'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
